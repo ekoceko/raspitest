14d20f592dbf8a6e3c8dd2135eccd201b135122e
Ekrem Pi <ekremcek19@hotmail.com>
2017-06-14 02:20:08 +0300

    initial code

---
+++ RasPiWorkspace/RaspiTests/src/main/java/pitest/App.java
@@ -0,0 +1,15 @@
+package pitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String t = "eko";
+        System.out.println( "this is "+t);
+    }
+}
